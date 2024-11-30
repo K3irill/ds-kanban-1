6d18c9cb8efde7ebf98e88f7bca477f6717b55ed
Kirill <razdor.2019@mail.ru>
2024-11-30 20:18:10 +0300

    feat: верстка Sidebar, компонент User

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,19 +1,74 @@
 import cn from 'classnames';
 import Image from 'next/image';
 import Link from 'next/link';
+import User from '@/components/user/User';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import styles from './Sidebar.module.scss';
+// ------------------------------------------------------------
+/* eslint-disable jsx-a11y/control-has-associated-label */
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(true);
+  const router = useRouter();
+
   return (
-    <aside className={cn(styles.sidebar)}>
+    <aside className={cn(styles.sidebar, { [styles['sidebar--open']]: isOpen })}>
       <div className={cn(styles.sidebar__wrapper)}>
         <div className={cn(styles.sidebar__content)}>
           <div className={cn(styles.sidebar__main)}>
-            <Link href="/">
-              <Image src="logo-white.svg" width={105} height={21} alt="logo" />
-            </Link>
+            <div className={cn(styles.sidebar__interactive)}>
+              {isOpen && (
+                <Link href="/">
+                  <Image src="logo-white.svg" width={105} height={21} alt="logo" />
+                </Link>
+              )}
+
+              <button
+                onClick={() => setIsOpen((prev) => !prev)}
+                className={cn(styles['sidebar__switch-btn'])}
+                type="button"
+              >
+                <svg className="social-icon" viewBox="0 0 16 16" width="16" height="16">
+                  <use href={`/sprite.svg#sidebar${isOpen ? 'Close' : 'Open'}`} />
+                </svg>
+              </button>
+            </div>
+            {isOpen && (
+              <div className={cn(styles['sidebar__user-info'])}>
+                <User
+                  user_avatar="/test.jpg"
+                  user_name="Админ Питоновский"
+                  user_position="Web-дизайнер"
+                />
+                <button
+                  onClick={() => router.push('/auth')}
+                  type="button"
+                  className={styles['sidebar__signout-btn']}
+                >
+                  Выйти
+                </button>
+              </div>
+            )}
           </div>
-          <div className={cn(styles.sidebar__projects)}>d</div>
+          <div
+            className={cn(styles.sidebar__links, {
+              [styles['sidebar__links--open']]: isOpen,
+            })}
+          >
+            {isOpen && <hr className={cn(styles['sidebar__hr-line'])} />}
+
+            <ul className={cn(styles.sidebar__links_list)}>
+              <li className={cn(styles.sidebar__links_item)}>
+                <Link href="/projects">
+                  <svg className="social-icon" viewBox="0 0 18 18" width="18" height="18">
+                    <use href="/sprite.svg#project" />
+                  </svg>
+                  {isOpen && <span>Проекты</span>}
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </aside>
