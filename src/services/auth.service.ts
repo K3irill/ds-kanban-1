d5b943aa0baf5fdbb4d8b40d0692af775cb8bd25
John Smith <666trall666@gmail.com>
2024-12-01 11:54:18 +0300

    feat: добавил валидацию типов в api  по zod

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,25 +1,41 @@
 import { axiosClassic, instance } from '@/api/axios';
-import { IUser, ILoginData } from '@/types/auth.type';
-import { saveTokenStorage } from './auth.helper';
+import {
+  IUser,
+  ILoginData,
+  UserSchema,
+  accessTokenShema,
+  TypeAccessToken,
+} from '@/types/auth.type';
 
-interface TypeAccessToken {
-  token: string;
-}
+import { ZodError } from 'zod';
+import { saveTokenStorage } from './auth.helper';
 
 class AuthService {
   static async login(data: ILoginData) {
     const response = await axiosClassic.post<TypeAccessToken>(
-      `/auth/token?email=${data.email}&password=${data.email}`,
+      `/auth/token?email=${data.email}&password=${data.password}`,
       data
     );
 
-    if (response.data.token) saveTokenStorage(response.data.token);
-    return response;
+    const fetchedToken = accessTokenShema.parse(response.data);
+
+    saveTokenStorage(fetchedToken.token);
+
+    return fetchedToken;
   }
 
   static async getUser() {
-    const response = await instance.get<IUser>(`/auth/user`);
-    return response;
+    const response = await instance.get<{ data: IUser }>(`/auth/user`);
+    try {
+      const fetchedUser = UserSchema.parse(response.data.data);
+
+      return fetchedUser;
+    } catch (error) {
+      if (error instanceof ZodError) {
+        console.error(error);
+      }
+      return error;
+    }
   }
 }
 
--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,136 +1,50 @@
-export interface IUser {
-  id: number;
-  is_active: boolean;
-  is_admin: boolean;
-  can_grade: boolean;
-  name: string;
-  surname: string;
-  patronymic: string;
-  email: string;
-  avatar: Avatar;
-  gender: Gender;
-  tags: Tag[];
-  projects: Project[];
-  created_at: string;
-  updated_at: string;
-}
+import { z } from 'zod';
 
-export interface Avatar {
-  id: number;
-  original_name: string;
-  link: string;
-  created_at: string;
-  updated_at: string;
-}
+export const iLoginDataShema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
-export interface Gender {
-  id: number;
-  name: string;
-}
+export const accessTokenShema = z.object({
+  token: z.string(),
+});
 
-export interface Tag {
-  id: number;
-  tag: string;
-  color_bg: string;
-  color_fg: string;
-}
+export const roleSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
 
-export interface Project {
-  id: number;
-  name: string;
-  wiki_link: string;
-  slug: string;
-  perm_manager_is_admin: boolean;
-  perm_user_create_task: boolean;
-  perm_user_self_assign: boolean;
-  capabilities: string[];
-  flow: Flow;
-  logo: Logo;
-  role: Role;
-  created_at: string;
-  updated_at: string;
-  begin: string;
-  end: string;
-  estimated_end: string;
-  project_planning: ProjectPlanning;
-  project_cost: ProjectCost;
-  project_completion: ProjectCompletion;
-}
+export const genderSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+});
 
-export interface Flow {
-  name: string;
-  slug: string;
-  visibleTaskFields: string[];
-  possibleProjectComponents: PossibleProjectComponent[];
-  possibleProjectStages: PossibleProjectStage[];
-}
+export const projectSchema = z.object({
+  id: z.number(),
+  capabilities: z.array(z.any()),
+  role: roleSchema,
+});
 
-export interface PossibleProjectComponent {
-  id: number;
-  name: string;
-}
-
-export interface PossibleProjectStage {
-  id: number;
-  name: string;
-}
-
-export interface Logo {
-  id: number;
-  original_name: string;
-  link: string;
-  created_at: string;
-  updated_at: string;
-}
-
-export interface Role {
-  id: number;
-  name: string;
-}
-
-export interface ProjectPlanning {
-  estimated: number;
-  planned: number;
-  planned_percent: number;
-}
-
-export interface ProjectCost {
-  summary: Summary;
-  items: Item[];
-}
-
-export interface Summary {
-  estimated_h: number;
-  fact_h: number;
-  estimated_total: number;
-  fact_total: number;
-  profit: number;
-  profitability: number;
-}
-
-export interface Item {
-  role: Role2;
-  estimated_h: number;
-  fact_h: number;
-  rate: number;
-  estimated_total: number;
-  fact_total: number;
-  deviation: number;
-  is_overtime: boolean;
-  is_warranty: boolean;
-}
-
-export interface Role2 {
-  id: number;
-  name: string;
-}
-
-export interface ProjectCompletion {
-  estimated: number;
-  completed: number;
-  completed_percent: number;
-}
-
-export interface ILoginData extends Pick<IUser, 'email'> {
-  password: string;
-}
+export const UserSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  surname: z.string(),
+  patronymic: z.string(),
+  position: z.string(),
+  is_active: z.boolean(),
+  is_admin: z.boolean(),
+  is_manager: z.boolean(),
+  email: z.string(),
+  projects: z.array(projectSchema),
+  gender: genderSchema,
+  avatar: z.any(),
+  telegram: z.string(),
+  created_at: z.string(),
+  updated_at: z.string(),
+  can_grade: z.boolean(),
+  nota_email: z.any(),
+});
+export type IUser = z.infer<typeof UserSchema>;
+export type Project = z.infer<typeof projectSchema>;
+export type ILoginData = z.infer<typeof iLoginDataShema>;
+export type TypeAccessToken = z.infer<typeof accessTokenShema>;
