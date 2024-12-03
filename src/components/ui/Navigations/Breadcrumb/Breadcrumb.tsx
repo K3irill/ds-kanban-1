8f6228453d32318d8e2b38ae41681ab5ef345e1c
Kirill <razdor.2019@mail.ru>
2024-12-03 14:13:38 +0300

    feat: чуть изменений, подправил страницу проектов и проектов через slug
    - вынес сайдбар и хлебные крошки в  отдельный layout
    - сделал loader
    - запрос к проекту через slug(пока без zod валидации)
    - новое свойство в eslint

--- src/components/ui/Navigations/Breadcrumb/Breadcrumb.tsx
+++ src/components/ui/Navigations/Breadcrumb/Breadcrumb.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 import { ReactNode } from 'react';
 import styles from './Breadcrumb.module.scss';
 
-interface BreadcrumbProps {
-  children: string | ReactNode;
+export interface BreadcrumbProps {
+  children?: string | ReactNode;
   href: string;
   className?: string;
   isActive?: boolean;
   isFirst?: boolean;
+  label?: string;
 }
 export default function Breadcrumb({
   children,
+  label,
   href,
   className,
   isActive,
---
+++ src/pages/projects/[slug]/index.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import React from 'react';
+import Head from 'next/head';
+import ProjectService from '@/services/project.service';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { Project } from '@/types/project.type';
+import ProjectLayout from '@/components/layout/Project/ProjectLayout';
+import cn from 'classnames';
+import { useRouter } from 'next/router';
+import Loader from '@/components/ui/Loader/loader';
+import styles from './KanbanPage.module.scss';
+
+const fetchProjectBySlug = async (slug: string): Promise<Project> =>
+  ProjectService.getProject(slug);
+
+export default function KanbanPage() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const projectSlug = Array.isArray(slug) ? slug[0] : slug;
+
+  const {
+    data: project,
+    isLoading,
+    error,
+  }: UseQueryResult<any, Error> = useQuery<any>({
+    queryKey: ['project', projectSlug],
+    queryFn: () => fetchProjectBySlug(projectSlug || ''),
+    enabled: !!projectSlug,
+  });
+
+  const breadcrumbs = [
+    { href: '/', label: 'Главная', isFirst: true },
+    { href: '/projects', label: 'Проекты', isActive: true },
+    ...(project ? [{ href: `/projects/${slug}`, label: project.name, isActive: true }] : []),
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>{project?.name || 'Загрузка...'}</title>
+        <meta name="description" content="Проект" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ProjectLayout breadcrumbs={breadcrumbs}>
+        {isLoading ? (
+          <div className={cn('loader-container')}>
+            <Loader />
+          </div>
+        ) : (
+          <div className={cn(styles['project-kanban'])}>
+            <div className={cn(styles['project-kanban__header'])}>
+              <h1>{project?.name || 'Загрузка...'}</h1>
+            </div>
+
+            <div className={cn(styles['project-kanban__tasks-container'])}>
+              {error && <p className={styles['error-message']}>Ошибка загрузки: {error.message}</p>}
+
+              {!isLoading && !error && (
+                <div className={cn(styles['project-kanban__projects-activities'])}>
+                  <p>содержимое проекта</p>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </ProjectLayout>
+    </>
+  );
+}
